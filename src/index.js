3e3eb5597f5c5b017bc7b9a56fbc4b3bc35e4524
AlionaMu <34584155+AlionaMu@users.noreply.github.com>
2020-02-07 20:11:22 +0300

    feat: test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
 
-// You should implement your task here.
-
-module.exports = function towelSort (matrix) {
-  return [];
-}
+/* You should implement your task here.
+module.exports = */
+/*var matrix = [[1,2,3],[4,5,6],[7,8,9]];*/
+function towelSort (matrix) {
+  /* var ix = matrix[x];*/
+   var final="";
+   var ix = 0;
+ while (ix <= matrix.length) {
+   if (ix % 2 == 0 || ix==0 ) {
+     for (var iy = 0; iy < matrix[ix].length; iy++){
+       final += matrix[ix][iy] + ", ";  
+   }
+     }
+   else {
+     
+     for (var iy = matrix[ix].length-1; iy >= 0 ; iy--){  
+       final += matrix[ix][iy] + ", ";
+     }
+   }
+   ix++; 
+   console.log (final);
+ 
+ }
+ return [final];
+ }
+ 
+ /*console.log (towelSort([[1,2,3],[4,5,6],[7,8,9]]));*/
+ 
+ 
+ 
