00916946a44a5fd258afa661f2cbd68c4622a7c7
AlionaMu <34584155+AlionaMu@users.noreply.github.com>
2020-02-10 15:16:57 +0300

    feat: test

--- src/index.js
+++ src/index.js
@@ -1,31 +1,27 @@
-
-/* You should implement your task here.
-module.exports = */
-/*var matrix = [[1,2,3],[4,5,6],[7,8,9]];*/
 function towelSort (matrix) {
-  /* var ix = matrix[x];*/
-   var final="";
-   var ix = 0;
- while (ix <= matrix.length) {
-   if (ix % 2 == 0 || ix==0 ) {
-     for (var iy = 0; iy < matrix[ix].length; iy++){
-       final += matrix[ix][iy] + ", ";  
-   }
-     }
-   else {
-     
-     for (var iy = matrix[ix].length-1; iy >= 0 ; iy--){  
-       final += matrix[ix][iy] + ", ";
-     }
-   }
-   ix++; 
-   console.log (final);
- 
+  const stack = [...matrix];
+  const res = [];
+  if (matrix.length == 0 || matrix == undefined) {
+    return [];
+  }
+  else {
+    var x = 0;
+ while (x < stack.length) {
+   if ( x==0 || x%2==0){
+    const next = stack[x];
+    res.push(...next);
+    x++;
  }
- return [final];
+ else {
+  const next = stack[x].reverse();
+  res.push(...next);
+  x++;
  }
- 
- /*console.log (towelSort([[1,2,3],[4,5,6],[7,8,9]]));*/
+ } 
+ return res;
+}
+}
+ console.log (towelSort([[1,2,3],[4,5,6],[7,8,9]]));
  
  
  
